5b22b6e11f36c72b89ef365ab00eaa82d4ebeb91
Techtainer <byungho.ahn@gmail.com>
2021-12-28 22:29:18 +0900

    12-5

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,18 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
-
+    let (query, filename) = parse_config(&args);
     println!("Searching for {}", query);
     println!("In file {}", filename);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong");
 
     println!("With text:\n{}", contents);
+}
+
+fn parse_config(arg: &[String]) -> (&str, &str) {
+    let query = &arg[1];
+    let filename = &arg[2];
+
+    (query, filename)
 }
