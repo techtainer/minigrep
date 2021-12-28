35ee3f04a4bd538fbd6e3680e0139a895e37d3dc
Techtainer <byungho.ahn@gmail.com>
2021-12-28 22:41:41 +0900

    12-10

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments:{}", err);
+        process::exit(1);
+    });
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
@@ -20,10 +24,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(arg: &[String]) -> Config {
+    fn new(arg: &[String]) -> Result<Config, &str> {
+        if arg.len() < 3 {
+            return Err("Not enough arguments");
+        }
         let query = arg[1].clone();
         let filename = arg[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
